799490b611a686b78775d130afa83b700839cba2
Andrew Furey <andrewfurey2003@gmail.com>
2025-08-31 18:50:57 +0100

    top1 and top5 accuracy scores

--- pretrain.py
+++ pretrain.py
@@ -70,47 +70,61 @@ if __name__ == "__main__":
     pretrain_model = model.YOLOv3tinyPretrain(num_classes).to(device)
     pretrain_model.train()
 
-    # optimizer
-    optim = torch.optim.AdamW(pretrain_model.parameters(), lr=lr, betas=adamw_betas, weight_decay=weight_decay) # TODO: set fused and capturable
-
-    # loss + learning rate scheduling
-    lossfn = torch.nn.CrossEntropyLoss()
+    # optimizer + lr scheduler
+    optim = torch.optim.SGD(pretrain_model.parameters(), lr=lr, weight_decay=weight_decay, momentum=momentum, nesterov=nesterov) # TODO: set fused and capturable
     lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer=optim, step_size=step_size, gamma=gamma)
+
+    # loss
+    lossfn = torch.nn.CrossEntropyLoss()
 
     # logging
     save_interval = 1000
     run_name = datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
 
-    # pretraining_log = wandb.init(
-    #     entity=os.getenv("ENTITY"),
-    #     project=os.getenv("PROJECT"),
-    #     name=run_name,
-    #     group="pretraining-imagenet",
-    #     mode="online",
-    # )
+    pretraining_log = wandb.init(
+        entity=os.getenv("ENTITY"),
+        project=os.getenv("PROJECT"),
+        name=run_name,
+        config={
+            "epochs":epochs,
+            "batch size":batch_size,
+            "learning rate scheduler":lr_scheduler.__class__.__name__,
+            "optimizer":optim.__class__.__name__,
+            "device":device.__str__()
+        },
+        group="pretraining-imagenet",
+        mode="online",
+    )
 
     if os.getenv("LOAD") != None:
         best_loss = True if int(os.getenv("LOAD")) == 1 else False # type: ignore
         checkpoint_file_name = get_latest_checkpoint(best_loss=best_loss)
         load = f"Using weights from checkpoint: {checkpoint_file_name}"
-        # pretraining_log.notes = load
+        # pretraining_log.notes += load
         # pretraining_log.save();
 
         pretrain_model.load_state_dict(torch.load(f"./{CHECKPOINTS}/{checkpoint_file_name}")["model"])
 
     # example
-    for epoch in range(epochs):
-        for (xbatch, ybatch) in tqdm(dataloader, desc=f"Training on epoch {epoch}"):
-            ypred = pretrain_model(xbatch)
-            loss = lossfn(ypred, ybatch)
-            loss.backward()
-            optim.step()
-            lr_scheduler.step()
-            # print("Loss: ", loss)
+    xbatch, ybatch = next(iter(dataloader))
+    # for epoch in [1]:
+    for epoch in trange(epochs):
+        # for (xbatch, ybatch) in tqdm(dataloader, desc=f"Training on epoch {epoch}"):
+        ypred = pretrain_model(xbatch)
+        loss = lossfn(ypred, ybatch)
+        loss.backward()
+        optim.step()
+        lr_scheduler.step()
+        pretraining_log.log({f"batch_loss_{0}": loss.item()})
 
-        # if epoch % save_interval == 0:
-        #     save_checkpoint(wandb.run.id, i, pretrain_model, optim, loss) # type: ignore
-        #     # TODO: validation
-        #     # pretraining_log.log({"val_loss": loss})
+        # calls torch.save. once every epoch, save weights.
+        # save_checkpoint(wandb.run.id, i, pretrain_model, optim, loss) # type: ignore
+        # validation, torch.no_grad
+        # pretraining_log.log({"val_loss": loss})
+        # top1 and top5
+        top1 = (ypred.argmax(dim=1) == ybatch).sum() / batch_size
+        top5 = (torch.topk(ypred, k=5, dim=1)[1] == ybatch.reshape(batch_size, 1)).max(dim=1)[0].float().sum() / batch_size
+        pretraining_log.log({f"top 1": top1})
+        pretraining_log.log({f"top 5": top5})
 
-    # pretraining_log.finish()
+    pretraining_log.finish()
